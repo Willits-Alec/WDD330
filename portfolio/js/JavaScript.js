b788dfd9013ff234e6227d1e26c32e622e669926
Willits-Alec <alecwillits@gmail.com>
2020-10-20 11:51:32 -0600

    changed name of js to main

--- portfolio/js/JavaScript.js
+++
@@ -1,9 +0,0 @@
-            const options = {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                year: "numeric"
-            };
-            const year = new Date();
-            document.getElementById("datetime").textContent = new Date().toDateString("en-us", options);
-            document.getElementById("year").innerHTML = year.getFullYear()
